21fdebadd70a8b366fb8911f68c998e943c82bd1
amirhosseinmohammadi114 <amirhossein.mohammadi114@gmail.com>
2025-11-04 23:05:43 +0330

    Add files via upload

---
+++ تمرین 7.cs
@@ -0,0 +1,35 @@
+﻿// tamrin 7
+// Amirhossein Mohammadi
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("adad asli ra vared konid");
+        int main = int.Parse(Console.ReadLine());
+
+        Console.Write("adad avval ra vared konid");
+        int a = int.Parse(Console.ReadLine());
+
+        Console.Write("adad dovvom ra vared konid");
+        int b = int.Parse(Console.ReadLine());
+
+        Console.Write("adad sevvom ra vared konid");
+        int c = int.Parse(Console.ReadLine());
+
+        int d1 = Math.Abs(main - a);
+        int d2 = Math.Abs(main - b);
+        int d3 = Math.Abs(main - c);
+
+        int closest = (d1, d2, d3) switch
+        {
+            var (x, y, z) when x <= y && x <= z => a,
+            var (x, y, z) when y <= x && y <= z => b,
+            _ => c
+        };
+
+        Console.WriteLine("nazdik tarin adad be adad asli" + closest);
+    }
+}
